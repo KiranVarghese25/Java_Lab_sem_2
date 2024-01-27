9453e5d804cee5bb26728fec821a7b570fcb2cab
KiranVarghese25 <140096768+KiranVarghese25@users.noreply.github.com>
2024-01-27 22:56:47 +0530

    Add files via upload

---
+++ Library_Management_sys/src/main/java/com/mycompany/library_management_sys/Library_Management_sys.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.library_management_sys;
+
+/**
+ *
+ * @author kiran
+ */
+public class Library_Management_sys {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
